8db005eeee45b3227269943039fa8df1ef288a19
TaeHwan Eun <theun0524@gmail.com>
2021-11-17 12:58:16 +0000

    fix: update xmpp

--- mix.exs
+++ mix.exs
@@ -109,7 +109,7 @@ defmodule Ejabberd.Mixfile do
      {:pkix, "~> 1.0"},
      {:stringprep, ">= 1.0.26", override: true},
      {:stun, "~> 1.0"},
-     {:xmpp, git: "https://github.com/vmeeting-io/xmpp.git", ref: "e45a72754f997613aa629a740529425ede41360d"},
+     {:xmpp, git: "https://github.com/vmeeting-io/xmpp.git", ref: "f0212037a5a298de05fdc1722d0046d061fdb21b"},
      {:yconf, "~> 1.0"},
      {:uuid, "~> 2.0", hex: :uuid_erl}]
     ++ cond_deps()
