153deaf4a84e43a417009914f3ce4718d86d830e
TaeHwan Eun <taehwan.eun@kedutech.kr>
2024-05-20 14:35:26 +0900

    fix: add transcriber to whitelist user

--- include/vmeeting_common.hrl
+++ include/vmeeting_common.hrl
@@ -1,2 +1,2 @@
 % TODO: get the whitelist value from config file
--define (WHITE_LIST_USERS, [<<"focus">>, <<"jvb">>, <<"jibri">>, <<"jvbbrewery">>, <<"jibribrewery">>, <<"recorder">>]).
+-define (WHITE_LIST_USERS, [<<"focus">>, <<"jvb">>, <<"jibri">>, <<"jvbbrewery">>, <<"jibribrewery">>, <<"recorder">>, <<"transcriber">>]).
